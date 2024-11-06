0eb3251bc5dae2375d5e621b090851ae2145ad95
joel <overfoxcompany@gmail.com>
2024-11-05 20:43:40 -0400

    first commit

---
+++ src/app/api/getData/route.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { v4 as uuidv4 } from 'uuid';
+export async function GET(req: Request) {
+    try {
+        return new Response(JSON.stringify({ message: 'Api funcionando', id: uuidv4() }))
+    } catch (error) {
+        console.log(error)
+        return new Response(JSON.stringify({ message: 'error en la api', error: error }))
+    }
+}
